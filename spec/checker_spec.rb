8793d11e8bbec3bc98944b73492d136cc3161e17
jocelynalsdorf <jfrodsla@gmail.com>
2015-04-26 08:19:32 -0700

    includes rspec#2 that  is commented out which tests that the method pushes repeated words into an array correctly;the method returns a number, this test returns an array and fails once method code is complete

--- spec/checker_spec.rb
+++ spec/checker_spec.rb
@@ -11,6 +11,11 @@ describe('String#checked') do
     expect("bad".checked(word)).to(eq(1))
   end
 
+# Test 2 passed, but now fails due to the method's output format once method's code completed
+ # it("returns the repeated words taken from a given sentence in an array") do
+   # expect(("a dog is dog and a cat").checked("dog")).to(eq(["dog", "dog"]))
+ # end
+
   it("counts number of times the word has been used by obtaining length of array") do
    expect(("this cat is cat and cat").checked("cat")).to(eq(3))
  end
